ad42ba5d9b55d729a5f4e348d87b968b56f713bc
Raquel Parronodo-Pizarro <raquelparrondo2000@gmail.com>
2025-11-10 12:07:33 +0100

    bash script to make predictions

---
+++ example_usage/ADME_public/scripts/model_building/chemprop_predict.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Activate the conda environment
+conda activate chemprop-env
+
+# Predict CLint endpoints with the corresponding ensemble
+chemprop predict \
+    --test-path ../data/CLint_dataset_with_splits.csv \
+    --model-paths ../CLint_model \
+    --preds-path ../CLint_model/predictions.csv \
+    --smiles-columns "Structure" \
+    --uncertainty-method ensemble
+
+# Predict PPB endpoints with the corresponding ensemble
+chemprop predict \
+    --test-path ../data/PPB_dataset_with_splits.csv \
+    --model-paths ../PPB_model \
+    --preds-path ../PPB_model/predictions.csv \
+    --smiles-columns "Structure" \
+    --uncertainty-method ensemble
+
+# Predict MDR1 endpoint with the corresponding ensemble
+chemprop predict \
+    --test-path ../data/MDR1_dataset_with_splits.csv \
+    --model-paths ../MDR1_model \
+    --preds-path ../MDR1_model/predictions.csv \
+    --smiles-columns "Structure" \
+    --uncertainty-method ensemble
+
